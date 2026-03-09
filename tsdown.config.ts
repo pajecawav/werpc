fb8ab238f87881c4fd274da00ee6b4c1f8517b9c
pajecawav <pajecawav@gmail.com>
2026-03-09 17:05:24 +0300

    feat: unbundle dist

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsdown";
 export default defineConfig(cfg => ({
 	entry: "./src/index.ts",
 	platform: "browser",
+	unbundle: true,
 	sourcemap: true,
 	dts: true,
 	tsconfig: "./tsconfig.lib.json",
